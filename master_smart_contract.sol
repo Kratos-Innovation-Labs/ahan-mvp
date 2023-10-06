df17d2943b7b0342563e7b232520775d79a5e477
Nishim Goyal <115455855+nishim3@users.noreply.github.com>
2023-10-06 19:56:02 +0530

    Update master_smart_contract.sol

--- master_smart_contract.sol
+++ master_smart_contract.sol
@@ -12,18 +12,18 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
-     uint256 public MINT_PRICE = 10000000000000 wei;//price of each NFT. Make sure this value is in wei and not ETH because of the transfer function.
-    uint public MAX_SUPPLY = 0;//Total number of NFTs. Increased when new farmers enter.
-    uint public tokens_per_farmer=100;//Number of tokens for each farmer.
-      uint public farmer_count;//Number of farmers
+     uint256 public MINT_PRICE = 10000000000000000 wei;
+    uint public MAX_SUPPLY = 0;
+    uint public tokens_per_farmer=100;
+      uint public farmer_count;
 
     struct Farmer
     {
-        address farmer_address;//farmer's wallet address
-        mapping(uint => string)  farmer_data;//CIDs associated with a farmer
-        uint noOfCID;//Number of CIDs
-        uint tokencount;//Number of tokens minted using the farmer's token ID.
-        uint amount;//Amount of money invested in the farmer by the investors.
+        address farmer_address;
+        mapping(uint => string)  farmer_data;
+        uint noOfCID;
+        uint tokencount;
+        uint amount;
     }
 
     constructor() ERC721("AHAN", "AHA")
@@ -31,24 +31,26 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         _tokenIdCounter.increment();
     }
 
-    mapping(uint => Farmer) public farmers;//array of farmers
+    mapping(uint => Farmer) public farmers;
+    mapping(uint => address) public investor;
+    uint public investor_count=0;
 
-    function addFarmer(address f_address) public//onboards farmers
+    function addFarmer(address f_address) public
     {
         farmer_count++;
         farmers[farmer_count].farmer_address=f_address;
         MAX_SUPPLY=MAX_SUPPLY+tokens_per_farmer;
-        _safeMint(farmers[farmer_count].farmer_address,(farmer_count-1)*tokens_per_farmer);//minting initial token(Master NFT)
+        _safeMint(farmers[farmer_count].farmer_address,(farmer_count-1)*tokens_per_farmer);
     }
 
     //function that adds CID
-    function addCID(uint farmerid,string memory cid) public//adding CIDs
+    function addCID(uint farmerid,string memory cid) public
     {
         farmers[farmerid].farmer_data[farmers[farmerid].noOfCID] = cid;
         farmers[farmerid].noOfCID++;
     }
     //function to get all the CIDS
-     function getData(uint farmerid) public view returns ( string[] memory) {//retrieving the CIDs
+     function getData(uint farmerid) public view returns ( string[] memory) {
         string[] memory cids = new string[](farmers[farmerid].noOfCID);
         for (uint i = 0; i < farmers[farmerid].noOfCID; i++) {
             cids[i] = farmers[farmerid].farmer_data[i];
@@ -56,7 +58,7 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return cids;
     }
 
-    function transferAmount(uint farmerid,uint percentage) public payable //transferring the percentage of money to farmer wallet
+    function transferAmount(uint farmerid,uint percentage) public payable 
     {
         require(farmerid<=farmer_count, "Farmer does not exist");
         require(farmers[farmerid].amount > 0, "Balance is zero");
@@ -65,20 +67,40 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         farmers[farmerid].amount=farmers[farmerid].amount-toPay;
     }
     
-    function safeMint(uint farmerid) public payable  {//minting NFTs, from contract to investors.
-                require(farmerid<=farmer_count, "Farmer does not exist");
-       require(totalSupply() < (MAX_SUPPLY+1), "Can't mint anymore tokens.");        
+    function safeMint(uint farmerid) public payable  {
+        require(farmerid<=farmer_count, "Farmer does not exist");
+        require(totalSupply() < (MAX_SUPPLY+1), "Can't mint anymore tokens.");        
         require(msg.value >= MINT_PRICE, "Not enough ether sent.");
         uint256 tokenId = (farmerid-1)*tokens_per_farmer+(farmers[farmerid].tokencount+1);
         farmers[farmerid].tokencount++;
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId,"https://bafkreibryhro5qkm5qebazepnib6xwn5zdx3xyjibmdonssavbktskeccq.ipfs.nftstorage.link/");
+
         farmers[farmerid].amount+=msg.value;
+        investor[++investor_count]=msg.sender;
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "ahan.ag/";
+    function investor_transfer() public 
+    {
+        for(uint i=1;i<=investor_count;i++)
+        {
+            require(address(this).balance>=112*MINT_PRICE/100,"No ether left in the treasury");
+            payable(investor[i]).transfer(112*MINT_PRICE/100);
+        }
+    }
+
+     function amount_to_deposit() public view returns(uint) 
+    {
+        return address(this).balance*12/100;
+    }
+    function deposit() public payable
+    {
+        
+    }
+    function withdraw() public 
+    {
+        payable(owner()).transfer(address(this).balance);
     }
 
     // The following functions are overrides required by Solidity.
@@ -111,4 +133,6 @@ contract MyToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     {
         return super.supportsInterface(interfaceId);
     }
+
+   
 }
